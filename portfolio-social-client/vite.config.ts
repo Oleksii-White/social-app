cd04e595ace8ebb8a1072d7cbab6121fac949216
Oleksii Bielykh <a.bielykh@gmail.com>
2025-08-11 14:41:02 +0200

    Updated cite-config

--- portfolio-social-client/vite.config.ts
+++ portfolio-social-client/vite.config.ts
@@ -1,15 +1,22 @@
-import { defineConfig } from "vite";
+// vite.config.ts
+import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
 import tsconfigPaths from "vite-tsconfig-paths";
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react(), tsconfigPaths()],
-  server: {
-    open: true,
-  },
-  build: {
-    outDir: "build",
-    sourcemap: true,
-  }
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), "");
+
+  return {
+    plugins: [react(), tsconfigPaths()],
+    server: {
+      open: true,
+    },
+    build: {
+      outDir: "build",
+      sourcemap: true,
+    },
+    define: {
+      "process.env.VITE_BASE_URL": JSON.stringify(env.VITE_BASE_URL),
+    },
+  };
 });
