ac85f5c6b97fe87b42a85ab67416e8bdce85123d
Oleksii Bielykh <a.bielykh@gmail.com>
2025-08-15 14:05:32 +0200

    changes to deploy on Fly

--- Dockerfile
+++ Dockerfile
@@ -3,12 +3,13 @@ WORKDIR /app/client
 COPY portfolio-social-client/package*.json ./
 RUN npm install
 COPY portfolio-social-client .
-ARG VITE_BASE_URL=""
-ENV VITE_BASE_URL=${VITE_BASE_URL}
+ARG VITE_BASE_URL
+ENV VITE_BASE_URL=$VITE_BASE_URL
 RUN npm run build
 
 FROM node:20 AS server-build
 WORKDIR /app/server
+RUN mkdir uploads
 COPY portfolio-social/package*.json ./
 RUN npm install
 COPY portfolio-social .
--- portfolio-social/routes/index.js
+++ portfolio-social/routes/index.js
@@ -10,7 +10,6 @@ const multer = require('multer');
 
 const uploadDestination = 'uploads';
 
-// Показываем, где хранить загружаемые файлы
 const storage = multer.diskStorage({
   destination: uploadDestination,
   filename: function (req, file, cb) {
@@ -19,28 +18,26 @@ const storage = multer.diskStorage({
 });
 
 const upload = multer({ storage: storage });
-// Роуты User
+//  User
 router.post("/register", UserController.register);
 router.post("/login", UserController.login);
 router.get("/current", authenticateToken, UserController.current);
 router.get("/users/:id", authenticateToken, UserController.getUserById);
 router.put("/users/:id", authenticateToken, upload.single('avatar'), UserController.updateUser);
 
-// Роуты Post
+//  Post
 router.post("/posts", authenticateToken, PostController.createPost);
 router.get("/posts", authenticateToken, PostController.getAllPosts);
 router.get("/posts/:id", authenticateToken, PostController.getPostById);
 router.delete("/posts/:id", authenticateToken, PostController.deletePost);
 
-// Роуты подписки
+
 router.post("/follow", authenticateToken, FollowController.followUser);
 router.delete("/unfollow/:id",authenticateToken, FollowController.unfollowUser);
 
-// Роуты лайков
 router.post("/likes", authenticateToken, LikeController.likePost);
 router.delete("/likes/:id", authenticateToken, LikeController.unlikePost);
 
-// Роуты комментариев
 router.post("/comments", authenticateToken, CommentController.createComment);
 router.delete(
   "/comments/:id",
