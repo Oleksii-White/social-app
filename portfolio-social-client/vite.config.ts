9aaaef3931e93231e5dd31e748a3170a6ac49ce1
Oleksii Bielykh <a.bielykh@gmail.com>
2025-08-15 14:21:17 +0200

    changes to deploy on Fly

--- portfolio-social-client/vite.config.ts
+++ portfolio-social-client/vite.config.ts
@@ -1,22 +1,31 @@
-// vite.config.ts
 import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
 import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd(), "");
+  
+  const isDevelopment = mode === "development";
 
   return {
     plugins: [react(), tsconfigPaths()],
     server: {
       open: true,
+      proxy: {
+        "/api": {
+          target: "http://localhost:3000",
+          changeOrigin: true,
+          secure: false,
+          rewrite: (path) => path.replace(/^\/api/, ""),
+        },
+      },
     },
     build: {
       outDir: "build",
       sourcemap: true,
     },
     define: {
-      "process.env.VITE_BASE_URL": JSON.stringify(env.VITE_BASE_URL),
+      "process.env.VITE_BASE_URL": isDevelopment ? JSON.stringify("/api") : JSON.stringify(env.VITE_BASE_URL),
     },
   };
 });
